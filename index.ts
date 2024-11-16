7f8cd378a1ce116c7a6e46c706a2711bc2a2df74
developerVictoria <zubannekoviktoriia@gmail.com>
2024-11-17 00:06:17 +0200

    added id handle

--- index.ts
+++ index.ts
@@ -1,21 +1,27 @@
 type MenuItem = {
+  id: number;
   name: string;
   price: number;
-}
+};
 //union
 type Status = "completed" | "ordered" | "in progress" ;
-//to make property option add ? before semicolon 
+//to make property option add ? before the colon 
 type Order =  {
   id: number;
   item: MenuItem;
   status: Status
-  }
+  };
+
+let cashRegister: number = 100;
+let nextOrderId: number = 1;
+let nextMenuItemId: number = 1;
+let orderQueue: Array<Order> = [];
 
 let menu: Array<MenuItem> = [
-  { name: 'Margarita', price: 1 },
-  { name: 'Peperoni', price: 2 },
-  { name: 'Hawaian', price: 3 },
-  { name: '4 Cheeses', price: 4 }
+  {id:nextMenuItemId++, name: 'Margarita', price: 1 },
+  {id:nextMenuItemId++, name: 'Peperoni', price: 2 },
+  {id:nextMenuItemId++, name: 'Hawaian', price: 3 },
+  {id:nextMenuItemId++, name: '4 Cheeses', price: 4 }
 ];
 
 //enum
@@ -25,16 +31,36 @@ enum orderStatus {
   INPROGRESS = "in progress"
 };
 
-let cashRegister: number = 100;
-let nextOrderId: number = 1;
-let orderQueue: Array<Order> = [];
-
-function addNewMenuItem(item: MenuItem) {
-  menu.push(item);
+//function return types  can be any, void, undentified, standart or custom datatypes
+function addNewMenuItem(item: Omit<MenuItem, "id">): MenuItem {
+  let newItem: MenuItem = { id:nextMenuItemId++, ...item}
+  menu.push(newItem);
+  return newItem;
 }
 
 
-function placeOrder(itemName : string) {
+function getMenuItemDetails(itemIdentifier: string | number): MenuItem | undefined{
+  let menuItem:MenuItem|undefined;
+
+  if(typeof itemIdentifier === "string"){
+    menuItem = menu.find((item)=>item.name.toLowerCase()===itemIdentifier.toLowerCase());
+  }else if(typeof itemIdentifier === "number"){
+    menuItem = menu.find((item)=>item.id===itemIdentifier);
+  }else{
+    throw new TypeError("Parameter identifier must be string or a number");
+  }
+  //TODO return the msg that claryfies that item doesn't exist in the menu
+  /*
+  if(!menuItem){
+    console.error(`this item: ${itemIdentifier} doesn't exists in the menu`);
+    return 
+  }
+*/    
+  return menuItem;
+  
+}
+
+function placeOrder(itemName : string): Order | undefined{
   const selectedItem = menu.find((itemObj) => itemObj.name === itemName);
   if(!selectedItem){
     console.error(`${itemName} doesn't exists in the menu`);
@@ -47,7 +73,7 @@ function placeOrder(itemName : string) {
   return newOrder;
 }
 
-function completeOrder(orderId: number) {
+function completeOrder(orderId: number): Order | undefined{
   let order = orderQueue.find((order) => order.id === orderId);
   if(!order){
     console.error(`№${orderId} order doesn't exist in the queue`);
@@ -58,10 +84,9 @@ function completeOrder(orderId: number) {
 }
 
 
-addNewMenuItem({name : 'BBQ', price: 5});
 
 placeOrder('BBQ');
-
+addNewMenuItem({name : 'BBQ', price: 5});
 completeOrder(1);
 
 console.log("Menu :", menu);
