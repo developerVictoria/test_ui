e3613e87457f7ca126ccf038ec600e1e33e109d0
developerVictoria <zubannekoviktoriia@gmail.com>
2024-11-16 21:25:54 +0200

    update to .ts

---
+++ index.ts
@@ -0,0 +1,66 @@
+type MenuItem = {
+  name: string;
+  price: number;
+}
+//to make property option add ? before semicolon 
+type Order =  {
+  id: number;
+  item: MenuItem;
+  status: string;
+  }
+
+let menu: MenuItem[] = [
+  { name: 'Margarita', price: 1 },
+  { name: 'Peperoni', price: 2 },
+  { name: 'Hawaian', price: 3 },
+  { name: '4 Cheeses', price: 4 }
+];
+
+const orderStatus = {
+  COMPLETED: 'completed',
+  ORDERED: 'ordered',
+  INPROGRESS: 'in progress'
+};
+
+let cashRegister: number = 100;
+let nextOrderId: number = 1;
+let orderQueue: Order[] = [];
+
+function addNewMenuItem(item: MenuItem) {
+  menu.push(item);
+}
+
+
+function placeOrder(itemName : string) {
+  const selectedItem = menu.find((itemObj) => itemObj.name === itemName);
+  if(!selectedItem){
+    console.error(`${itemName} doesn't exists in the menu`);
+    return;
+  }
+  cashRegister += selectedItem.price;
+  const newOrder = { id: nextOrderId++, item: selectedItem, status: orderStatus.ORDERED };
+
+  orderQueue.push(newOrder);
+  return newOrder;
+}
+
+function completeOrder(orderId: number) {
+  let order = orderQueue.find((order) => order.id === orderId);
+  if(!order){
+    console.error(`№${orderId} order doesn't exist in the queue`);
+    return
+  }
+  order.status = orderStatus.COMPLETED;
+  return order;
+}
+
+
+addNewMenuItem({name : 'BBQ', price: 5});
+
+placeOrder('BBQ');
+
+completeOrder(1);
+
+console.log("Menu :", menu);
+console.log("Cash in register :", cashRegister);
+console.log("Order queue :", orderQueue);
